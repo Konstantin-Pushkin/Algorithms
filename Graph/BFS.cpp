d633f9bec5e932b10f5c7ae6e4b1da024d5451e6
Kostja <kostja10768@gmail.com>
2024-07-14 14:17:29 +0300

    Adding BFS

---
+++ Graph/BFS.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <unordered_map>
+#include <list>
+#include <queue>
+#include <cstdint>
+#include <algorithm>
+
+void addingEdgeToGraph(std::unordered_map<uint64_t, std::list<uint64_t>> &graph, uint64_t currentVertex, uint64_t vertexNeighbor)
+{
+    if(graph.find(currentVertex) == graph.end())
+    {
+        graph[currentVertex] = std::list<uint64_t>();
+    }
+
+    if(graph.find(vertexNeighbor) == graph.end())
+    {
+        graph[vertexNeighbor] = std::list<uint64_t>();
+    }
+
+    if(std::find(graph[currentVertex].begin(), graph[currentVertex].end(), vertexNeighbor) == graph[currentVertex].end())
+    {
+        graph[currentVertex].push_back(vertexNeighbor);
+    }
+}
+
+void printGraph(std::unordered_map<u_int64_t, std::list<u_int64_t>> &graph)
+{
+    for(const auto &pair : graph)
+    {
+        std::cout << pair.first << ": ";
+        for(const auto &neighbor : pair.second)
+        {
+            std::cout << neighbor << " ";
+        }
+
+        std::cout << std::endl;
+    }
+}
+
+void BFS(std::unordered_map<u_int64_t, std::list<u_int64_t>> &graph, u_int64_t start)
+{
+    std::unordered_map<u_int64_t, bool> visited;
+    std::queue<u_int64_t> queue;
+
+    for(const auto &pair : graph)
+    {
+        visited[pair.first] = false;
+    }
+
+    visited[start] = true;
+    queue.push(start);
+
+    while(!queue.empty())
+    {
+        u_int64_t vertex = queue.front();
+        std::cout << vertex << " ";
+        queue.pop();
+
+        if(graph.find(vertex) != graph.end())
+        {
+            for(const uint64_t &neighbor: graph.at(vertex))
+            {
+                if(!visited[neighbor])
+                {
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    std::unordered_map<u_int64_t, std::list<u_int64_t>> graph;
+    addingEdgeToGraph(graph, 0, 1);
+    addingEdgeToGraph(graph, 0, 2);
+    addingEdgeToGraph(graph, 0, 3);
+    addingEdgeToGraph(graph, 1, 4);
+    addingEdgeToGraph(graph, 2, 5);
+    addingEdgeToGraph(graph, 2, 9);
+
+    std::cout << "Vertices: ";
+    BFS(graph, 0);
+    std::cout << std::endl;
+
+    printGraph(graph);
+
+    return EXIT_SUCCESS;
+}
